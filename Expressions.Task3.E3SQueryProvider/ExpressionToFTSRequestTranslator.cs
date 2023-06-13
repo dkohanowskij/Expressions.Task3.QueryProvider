dce13f71fe63c57159f1684698b99ff2744377a9
Dzmitriy Kakhanouski <dzmitriy.kakhanouski@thomsonreuters.com>
2023-06-13 10:20:53 +0400

    Add project files.

---
+++ Expressions.Task3.E3SQueryProvider/ExpressionToFTSRequestTranslator.cs
@@ -0,0 +1,121 @@
+﻿using Expressions.Task3.E3SQueryProvider.Models.Request;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Expressions.Task3.E3SQueryProvider
+{
+    public class ExpressionToFtsRequestTranslator : ExpressionVisitor
+    {
+        readonly StringBuilder _resultStringBuilder;
+
+        public ExpressionToFtsRequestTranslator()
+        {
+            _resultStringBuilder = new StringBuilder();
+        }
+
+        public string Translate(Expression exp)
+        {
+            Visit(exp);
+
+            return _resultStringBuilder.ToString();
+        }
+
+        #region protected methods
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if (node.Method.DeclaringType == typeof(Queryable)
+                && node.Method.Name == "Where")
+            {
+                var predicate = node.Arguments[1];
+                Visit(predicate);
+
+                return node;
+            }
+
+            if (node.Method.DeclaringType == typeof(string))
+            {
+                Visit(node.Object as MemberExpression);
+
+                if (node.Method.Name == "Equals")
+                {
+                    Visit(node.Arguments[0]);
+                    return node;
+                }
+
+                var constantExpression = node.Arguments[0] as ConstantExpression;
+                StringBuilder sb = new StringBuilder(constantExpression.Value.ToString());
+
+                if (node.Method.Name == "StartsWith")
+                {
+                    sb.Append('*');
+                }
+
+                if (node.Method.Name == "EndsWith")
+                {
+                    sb.Insert(0, '*');
+                }
+
+                if (node.Method.Name == "Contains")
+                {
+                    sb.Insert(0, '*');
+                    sb.Append('*');
+                }
+
+                constantExpression = Expression.Constant(sb.ToString(), typeof(string));
+                Visit(constantExpression);
+                return node;
+            }
+
+            return base.VisitMethodCall(node);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            switch (node.NodeType)
+            {
+                case ExpressionType.Equal:
+                    Visit(node.Right);
+                    Visit(node.Left);
+                    break;
+                case ExpressionType.AndAlso: 
+                    var statements = new List<Statement>();
+                    Visit(node.Left);
+                    statements.Add(new Statement { Query = _resultStringBuilder.ToString() });
+                    _resultStringBuilder.Clear();
+                    Visit(node.Right);
+                    statements.Add(new Statement { Query = _resultStringBuilder.ToString() });
+                    _resultStringBuilder.Clear();
+                    _resultStringBuilder.Append(JsonConvert.SerializeObject(new { statements = statements }));
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Operation '{node.NodeType}' is not supported");
+            };
+
+            return node;
+        }
+
+        protected override Expression VisitMember(MemberExpression node)
+        {
+            _resultStringBuilder.Insert(0, node.Member.Name).Insert(node.Member.Name.Length, ":");
+
+            return base.VisitMember(node);
+        }
+
+        protected override Expression VisitConstant(ConstantExpression node)
+        {
+            _resultStringBuilder.Append("(");
+            _resultStringBuilder.Append(node.Value);
+            _resultStringBuilder.Append(")");
+
+            return node;
+        }
+
+        #endregion
+    }
+}
---
+++ Expressions.Task3.E3SQueryProvider/QueryProvider/E3SEntitySet.cs
@@ -0,0 +1,51 @@
+﻿using Expressions.Task3.E3SQueryProvider.Services;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Expressions.Task3.E3SQueryProvider.Models.Entities;
+
+namespace Expressions.Task3.E3SQueryProvider.QueryProvider
+{
+    public class E3SEntitySet<T> : IQueryable<T> where T : BaseE3SEntity
+    {
+        protected readonly Expression _expr;
+        protected readonly IQueryProvider _queryProvider;
+
+        public E3SEntitySet(E3SSearchService client)
+        {
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            _expr = Expression.Constant(this);
+            _queryProvider = new E3SLinqProvider(client);
+        }
+
+        #region public properties
+
+        public Type ElementType => typeof(T);
+
+        public Expression Expression => _expr;
+
+        public IQueryProvider Provider => _queryProvider;
+
+        #endregion
+
+        #region public methods
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _queryProvider.Execute<IEnumerable<T>>(_expr).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _queryProvider.Execute<IEnumerable>(_expr).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
